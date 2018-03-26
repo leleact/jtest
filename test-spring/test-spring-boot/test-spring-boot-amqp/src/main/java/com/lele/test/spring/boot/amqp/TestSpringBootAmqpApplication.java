3168acd0f4c3ee72473a50464dbd9094ebf37fef
leleact <leleact@gmail.com>
2018-03-26 11:10:41 +0800

    spring boot amqp test

---
+++ test-spring/test-spring-boot/test-spring-boot-amqp/src/main/java/com/lele/test/spring/boot/amqp/TestSpringBootAmqpApplication.java
@@ -0,0 +1,12 @@
+package com.lele.test.spring.boot.amqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootAmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootAmqpApplication.class, args);
+	}
+}
