a0d8de2c52088046ce2002a27fa115d2efd334bb
leleact <leleact@gmail.com>
2018-03-27 22:20:16 +0800

    spring boot tx test

--- test-spring/test-spring-boot/test-spring-boot-mybatis/src/main/java/com/lele/test/spring/boot/mybatis/service/TransactionService.java
+++ test-spring/test-spring-boot/test-spring-boot-mybatis/src/main/java/com/lele/test/spring/boot/mybatis/service/TransactionService.java
@@ -18,6 +18,30 @@ public class TransactionService {
         return t1Mapper.insertSelective(t1);
     }
 
+    /**
+     * 外部调用insertWrapper时, insertWrapper内部调用的insert上的 @Transactional不生效
+     * <p></p>
+     * 如果加上@Transactional,外部调用时,@Transactional生效,但是 insert(t1)为什么不是调用的代理类方法,
+     * 而是被代理的类??? 对比<code>@Configuration</code>下面的@Bean注解的方法, 如果一个bean依赖另外一个bean
+     * <pre>{@code
+     *    @literal @Configuration
+     *     public class ConfigurationBean {
+     *        @literal @Bean
+     *         public Bean1 bean1() {
+     *            return new Bean1();
+     *         }
+     *
+     *        @literal @Bean
+     *         public Bean2 bean2() {
+     *             Bean2 b2 = new Bean2();
+     *             b2.setBean1(bean1());
+     *             return b2;
+     *         }
+     *     };
+     * }</pre>
+     * <code>b2.setBean1(bean1())</code>中<coode>bean1()</coode>调用的是代理类方法
+     */
+    // @Transactional(propagation= Propagation.REQUIRES_NEW)
     public int insertWrapper(T1 t1) {
         return insert(t1);
     }
