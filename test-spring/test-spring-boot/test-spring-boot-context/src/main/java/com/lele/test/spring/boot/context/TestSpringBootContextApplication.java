c52b5c6009d08e4625c34eda1b9d8fd94b31fc48
leleact <leleact@gmail.com>
2018-03-29 22:00:03 +0800

    spring context test

---
+++ test-spring/test-spring-boot/test-spring-boot-context/src/main/java/com/lele/test/spring/boot/context/TestSpringBootContextApplication.java
@@ -0,0 +1,12 @@
+package com.lele.test.spring.boot.context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBootContextApplication.class, args);
+    }
+}
---
+++ test-spring/test-spring-boot/test-spring-boot-context/src/main/java/com/lele/test/spring/boot/context/service/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,23 @@
+package com.lele.test.spring.boot.context.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    private static final Logger log = LoggerFactory.getLogger(MyBeanFactoryPostProcessor.class);
+
+    public MyBeanFactoryPostProcessor() {
+        log.info("==================  constructor ================");
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        log.info("==============={}==============", beanFactory);
+    }
+}
