06ff5f48f7a014f8ea5b44ab7b2e328812cd477c
leleact <leleact@gmail.com>
2018-04-09 15:05:29 +0800

    dubbo starter test

---
+++ test-dubbo/test-dubbo-starter/src/main/java/com/lele/test/dubbo/starter/TestDubboStarterApplication.java
@@ -0,0 +1,13 @@
+package com.lele.test.dubbo.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDubboStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDubboStarterApplication.class, args);
+    }
+
+}
---
+++ test-dubbo/test-dubbo-starter/src/test/java/com/lele/test/dubbo/starter/TestDubboStarterApplicationTests.java
@@ -0,0 +1,21 @@
+package com.lele.test.dubbo.starter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestDubboStarterApplicationTests {
+
+    private static final Logger log = LoggerFactory.getLogger(TestDubboStarterApplicationTests.class);
+
+    @Test
+    public void contextTest() {
+
+    }
+
+}
