bd6ca6ab662b93a4cd4c20bfbdc7e24858f2c223
leleact <leleact@gmail.com>
2018-04-09 15:50:54 +0800

    log4j2.skipJansi test

--- test-log4j2/src/test/java/com/lele/test/log4j2/TestLog4j2ApplicationTests.java
+++ test-log4j2/src/test/java/com/lele/test/log4j2/TestLog4j2ApplicationTests.java
@@ -6,12 +6,17 @@ import org.slf4j.LoggerFactory;
 
 public class TestLog4j2ApplicationTests {
 
+//    static {
+//        System.setProperty("log4j2.skipJansi", "false");
+//    }
+
     private static final Logger log = LoggerFactory.getLogger(TestLog4j2ApplicationTests.class);
 
     private static final Logger anotherLogger = LoggerFactory.getLogger("com.another");
 
     @Test
     public void log4j2Test() {
+
 
         log.trace("trace");
         log.debug("debug");
