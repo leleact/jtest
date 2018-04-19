9e3740b768be049de58d4e4aaed185e688c07d45
leleact <leleact@gmail.com>
2018-04-19 10:57:58 +0800

    move DemoService

---
+++ test-dubbo/test-dubbo-compatibility/test-dubbo-compatibility-provider/src/main/java/com/lele/test/dubbo/compatibility/provider/dubbo/service/impl/EchoServiceImpl.java
@@ -0,0 +1,18 @@
+package com.lele.test.dubbo.compatibility.provider.dubbo.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.lele.test.dubbo.compatibility.api.EchoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class EchoServiceImpl implements EchoService {
+
+    private static final Logger log = LoggerFactory.getLogger(EchoServiceImpl.class);
+
+    @Override
+    public String echo(String message) {
+        log.info("receive message: {}", message);
+        return message;
+    }
+}
