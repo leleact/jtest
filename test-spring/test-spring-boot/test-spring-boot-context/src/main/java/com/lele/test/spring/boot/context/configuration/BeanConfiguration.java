c6ee64a8733463df5f02e2c8b106a7dae41e8a6f
leleact <leleact@gmail.com>
2018-04-19 13:42:49 +0800

    bean override

---
+++ test-spring/test-spring-boot/test-spring-boot-context/src/main/java/com/lele/test/spring/boot/context/configuration/BeanConfiguration.java
@@ -0,0 +1,33 @@
+package com.lele.test.spring.boot.context.configuration;
+
+import com.lele.test.spring.boot.context.bean.Person;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration implements ApplicationContextAware  {
+
+    private ApplicationContext applicationContext;
+
+    // doCreateBean
+    // mbd.getFactoryMethodName() != null
+    // instantiateUsingFactoryMethod
+    @Bean("p")
+    public Person p() {
+        return new Person();
+    }
+
+    // Bean named P created by p1() will override bean created by p()
+    @Bean("p")
+    public Person p1() {
+        return new Person();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
