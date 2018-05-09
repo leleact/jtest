b71054fb67718fc22013ae9bd26620ec96c07d97
leleact <leleact@gmail.com>
2018-05-09 17:25:21 +0800

    jdk collection test

---
+++ test-jdk/test-jdk-collection/src/test/java/com/lele/test/jdk/collection/DequeTests.java
@@ -0,0 +1,38 @@
+package com.lele.test.jdk.collection;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.NoSuchElementException;
+
+public class DequeTests {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void pollTest() {
+
+        Deque<String> deque = new ArrayDeque<>();
+
+        {
+            deque.push("1");
+            deque.push("2");
+
+            String str = deque.poll();
+
+            Assert.assertEquals("2", str);
+            Assert.assertEquals(1, deque.size());
+        }
+
+        {
+            deque.pop();
+            thrown.expect(NoSuchElementException.class);
+            deque.pop();
+        }
+    }
+}
