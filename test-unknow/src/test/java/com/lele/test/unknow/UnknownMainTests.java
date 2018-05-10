22121cce90c94bf26ea49a20d0b487ffdb6ecafb
leleact <leleact@gmail.com>
2018-05-10 21:47:17 +0800

    CountDownLatch test

--- test-unknow/src/test/java/com/lele/test/unknow/UnknownMainTests.java
+++ test-unknow/src/test/java/com/lele/test/unknow/UnknownMainTests.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class UnknowMainTests {
+import java.util.concurrent.CountDownLatch;
+
+public class UnknownMainTests {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Test
     public void mainTest() {
-        log.info("Hello World");
-        log.error("Hello World");
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        countDownLatch.countDown();
     }
 }
