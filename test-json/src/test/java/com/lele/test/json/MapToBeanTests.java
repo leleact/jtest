8aee92d53a2bac4ad38857c832743aefe2f0edf8
leleact <leleact@gmail.com>
2018-05-24 22:12:48 +0800

    map to bean

---
+++ test-json/src/test/java/com/lele/test/json/MapToBeanTests.java
@@ -0,0 +1,48 @@
+package com.lele.test.json;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class MapToBeanTests {
+
+    public static class TestBean {
+
+        private String id;
+
+        private Integer count;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public Integer getCount() {
+            return count;
+        }
+
+        public void setCount(Integer count) {
+            this.count = count;
+        }
+    };
+
+    @Test
+    public void mapToBeanTest() {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", UUID.randomUUID().toString().replace("-", ""));
+        map.put("count", 32);
+
+        JSONObject object = new JSONObject(map);
+        TestBean bean = object.toJavaObject(TestBean.class);
+        Assert.assertEquals(32, bean.getId().length());
+        Assert.assertEquals(32L, (long)bean.getCount());
+    }
+}
