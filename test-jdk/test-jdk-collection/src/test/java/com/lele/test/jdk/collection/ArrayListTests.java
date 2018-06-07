d2572c71f561b3e29b8a31b9eb18d2e267e07e8b
leleact <leleact@gmail.com>
2018-06-07 19:44:37 +0800

    array list add null object test

---
+++ test-jdk/test-jdk-collection/src/test/java/com/lele/test/jdk/collection/ArrayListTests.java
@@ -0,0 +1,24 @@
+package com.lele.test.jdk.collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTests {
+
+    private static final Logger log = LoggerFactory.getLogger(ArrayListTests.class);
+
+    @Test
+    public void addNullObjectTest() {
+
+        List<String> list = new ArrayList<>();
+
+        list.add(null);
+
+        Assert.assertEquals(1, list.size());
+    }
+}
