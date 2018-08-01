27a3b01f3105b6fb469191dae280908f81a2c87d
leleact <leleact@gmail.com>
2018-08-01 20:01:58 +0800

    RequestBody test

---
+++ test-spring/test-spring-boot/test-spring-boot-web/src/main/java/com/lele/test/spring/boot/web/controller/XmlController.java
@@ -0,0 +1,21 @@
+package com.lele.test.spring.boot.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class XmlController {
+
+    private static final Logger log = LoggerFactory.getLogger(XmlController.class);
+
+    @RequestMapping("/xml")
+    public String xml(@RequestBody String xml, @RequestBody byte[] bytes) {
+        log.info("receive:[{}]", xml);
+
+        log.info("byte: [{}]", new String(bytes));
+        return "0000";
+    }
+}
