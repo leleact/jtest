e66ad2f7550164cdaee652f7ba85f7bfe0e75086
leleact <leleact@gmail.com>
2019-04-18 17:31:35 +0800

    spring: add spring jdbc test

---
+++ test-spring/spring-jdbc/src/test/resources/schema.sql
@@ -0,0 +1,13 @@
+drop TABLE IF EXISTS person;
+
+create table if not exists person
+(
+    id  int auto_increment primary key,
+    name varchar(20) null,
+    age  int         null
+);
+
+insert into person (name, age)
+values ('a', 1),
+       ('b', 2),
+       ('c', 3);
