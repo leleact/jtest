5a84916a77c8ddbde5452da18391592b108efb07
leleact <leleact@gmail.com>
2019-05-16 20:40:43 +0800

    maven plugin: google check style

---
+++ maven/maven-plugin/maven-plugin-checkstyle/maven-plugin-google-checkstyle/src/main/java/com/github/leleact/jtest/maven/plugin/checkstyle/spring/MavenPluginCheckStyleGoogleApplication.java
@@ -0,0 +1,12 @@
+package com.github.leleact.jtest.maven.plugin.checkstyle.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPluginCheckStyleGoogleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenPluginCheckStyleGoogleApplication.class, args);
+    }
+}
--- maven/maven-plugin/maven-plugin-checkstyle/maven-plugin-spring-checkstyle/src/main/java/com/github/leleact/jtest/maven/plugin/checkstyle/spring/MavenPluginCheckStyleSpringApplication.java
+++ maven/maven-plugin/maven-plugin-checkstyle/maven-plugin-spring-checkstyle/src/main/java/com/github/leleact/jtest/maven/plugin/checkstyle/spring/MavenPluginCheckStyleSpringApplication.java
@@ -1,4 +1,4 @@
-package com.github.leleact.jtest.maven.plugin.checkstyle;
+package com.github.leleact.jtest.maven.plugin.checkstyle.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,14 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author lele alien
  */
 @SpringBootApplication
-public class MavenPluginCheckStyleApplication {
+public class MavenPluginCheckStyleSpringApplication {
 
 	/**
 	 * program entry.
 	 * @param args main entry arguments
 	 */
 	public static void main(String[] args) {
-		SpringApplication.run(MavenPluginCheckStyleApplication.class, args);
+		SpringApplication.run(MavenPluginCheckStyleSpringApplication.class, args);
 	}
 
 }
