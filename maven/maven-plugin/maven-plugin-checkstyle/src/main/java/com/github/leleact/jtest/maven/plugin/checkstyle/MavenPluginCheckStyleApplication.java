a4ae77e991f4e276d364a8361e17a675e342cbb0
leleact <leleact@gmail.com>
2019-05-16 14:50:00 +0800

    maven plugin: checkstyle plugin use spring checkstyle

--- maven/maven-plugin/maven-plugin-checkstyle/src/main/java/com/github/leleact/jtest/maven/plugin/checkstyle/MavenPluginCheckStyleApplication.java
+++ maven/maven-plugin/maven-plugin-checkstyle/src/main/java/com/github/leleact/jtest/maven/plugin/checkstyle/MavenPluginCheckStyleApplication.java
@@ -3,10 +3,20 @@ package com.github.leleact.jtest.maven.plugin.checkstyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * main.
+ *
+ * @author lele alien
+ */
 @SpringBootApplication
 public class MavenPluginCheckStyleApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MavenPluginCheckStyleApplication.class, args);
-    }
+	/**
+	 * program entry.
+	 * @param args main entry arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPluginCheckStyleApplication.class, args);
+	}
+
 }
