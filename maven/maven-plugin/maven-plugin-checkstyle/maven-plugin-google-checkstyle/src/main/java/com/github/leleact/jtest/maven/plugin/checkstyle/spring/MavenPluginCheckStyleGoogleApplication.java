2b832e9758cc4f70f1e741988367e1314252dc90
leleact <leleact@gmail.com>
2019-05-17 09:39:35 +0800

    maven plugin: checkstyle plugin use maven properties

--- maven/maven-plugin/maven-plugin-checkstyle/maven-plugin-google-checkstyle/src/main/java/com/github/leleact/jtest/maven/plugin/checkstyle/spring/MavenPluginCheckStyleGoogleApplication.java
+++ maven/maven-plugin/maven-plugin-checkstyle/maven-plugin-google-checkstyle/src/main/java/com/github/leleact/jtest/maven/plugin/checkstyle/spring/MavenPluginCheckStyleGoogleApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MavenPluginCheckStyleGoogleApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(MavenPluginCheckStyleGoogleApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MavenPluginCheckStyleGoogleApplication.class, args);
+  }
 }
