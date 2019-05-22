dbc3942bff841b00e13ac448a5928bab2e3d2307
leleact <leleact@gmail.com>
2019-05-22 17:36:41 +0800

    jtest: reorganize pom.xml

--- feature/test-jdk-resource/src/test/java/com/lele/test/jdk/resource/LoadPropertiesTests.java
+++ feature/test-jdk-resource/src/test/java/com/lele/test/jdk/resource/LoadPropertiesTests.java
@@ -1,42 +1,41 @@
 package com.lele.test.jdk.resource;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-public class LoadPropertiesTests {
+class LoadPropertiesTests {
 
     @Test
-    public void loadPropertiesWithClassAbsolutePath() throws IOException {
+    void loadPropertiesWithClassAbsolutePath() throws IOException {
         // 绝对路径
         InputStream inputStream = this.getClass().getResourceAsStream("/config.properties");
         Properties properties = new Properties();
         properties.load(inputStream);
-        Assert.assertEquals("1", properties.getProperty("a"));
-        Assert.assertEquals("2", properties.getProperty("b"));
+        Assertions.assertEquals("1", properties.getProperty("a"));
+        Assertions.assertEquals("2", properties.getProperty("b"));
     }
 
     @Test
-    public void loadPropertiesWithClassRelativePath() throws IOException {
+    void loadPropertiesWithClassRelativePath() throws IOException {
         // 相对路径
         InputStream inputStream = this.getClass().getResourceAsStream("../../../../../config.properties");
         Properties properties = new Properties();
         properties.load(inputStream);
-        Assert.assertEquals("1", properties.getProperty("a"));
-        Assert.assertEquals("2", properties.getProperty("b"));
+        Assertions.assertEquals("1", properties.getProperty("a"));
+        Assertions.assertEquals("2", properties.getProperty("b"));
     }
 
     @Test
-    public void loadPropertiesWithClassLoader() throws IOException {
+    void loadPropertiesWithClassLoader() throws IOException {
         // 只有绝对路径
         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("config.properties");
         Properties properties = new Properties();
         properties.load(inputStream);
-        Assert.assertEquals("1", properties.getProperty("a"));
-        Assert.assertEquals("2", properties.getProperty("b"));
+        Assertions.assertEquals("1", properties.getProperty("a"));
+        Assertions.assertEquals("2", properties.getProperty("b"));
     }
-
 }
