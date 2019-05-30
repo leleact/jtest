4427a89371f4cd31037393150d4a30073048ab62
leleact <leleact@gmail.com>
2019-05-30 14:42:10 +0800

    jdk: artifactId feature to jdk

---
+++ jdk/jdk-reflect/src/main/java/com/github/leleact/jtest/jdk/reflect/Executor.java
@@ -0,0 +1,14 @@
+package com.github.leleact.jtest.jdk.reflect;
+
+/**
+ * Executor.
+ *
+ * @author leleact
+ * @since 1.0
+ */
+public class Executor {
+
+    public void execute() throws InterruptedException {
+        Thread.sleep(1);
+    }
+}
