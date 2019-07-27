116da2a810dc19b6e96e28ae112708ed1b3f7af8
leleact <leleact@gmail.com>
2019-07-27 10:57:59 +0800

    logback: log auto delete

---
+++ logback/logback-delete/src/test/java/com/github/leleact/jtest/logback/delete/GenLogTests.java
@@ -0,0 +1,19 @@
+package com.github.leleact.jtest.logback.delete;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.RepeatedTest;
+
+/**
+ * generate log.
+ *
+ * @author Cai Pan
+ * @since 1.0
+ */
+@Slf4j
+class GenLogTests {
+
+    @RepeatedTest(100)
+    void genLogTest() {
+        log.debug("1");
+    }
+}
