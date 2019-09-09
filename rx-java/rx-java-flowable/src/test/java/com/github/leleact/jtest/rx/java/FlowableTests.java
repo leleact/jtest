654e7be5da44be71043b3c9938986c375a257701
leleact <leleact@gmail.com>
2019-09-09 16:33:34 +0800

    feat(rx-java): async flowable test
    async flowable test

--- rx-java/rx-java-flowable/src/test/java/com/github/leleact/jtest/rx/java/FlowableTests.java
+++ rx-java/rx-java-flowable/src/test/java/com/github/leleact/jtest/rx/java/FlowableTests.java
@@ -1,6 +1,7 @@
 package com.github.leleact.jtest.rx.java;
 
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,4 +15,35 @@ public class FlowableTests {
         log.info("Thread Name");
         Flowable.just("Hello world").subscribe(log::info);
     }
+
+    @Test
+    public void flatMapTest() {
+        Flowable.just(1, 2)
+                .flatMap(it -> Flowable.just(it)
+                                       .subscribeOn(Schedulers.computation())//控制在哪些线程上生成sub-stream
+                                       .map(i -> {
+                                           System.out.println(i + "  thread: " + Thread.currentThread());
+                                           return i;
+                                       }))
+                .subscribe(it -> {
+                    System.out.println("onNext:" + it + "  thread: " + Thread.currentThread());
+                });
+
+    }
+
+    @Test
+    public void parallelTest() {
+        Flowable.just(1, 2)
+                .parallel()
+                .runOn(Schedulers.io())//指定在哪些线程上并发执行
+                .map(it -> {
+                    System.out.println(it + "  thread: " + Thread.currentThread());
+                    return it;
+                })
+                .sequential()
+                .subscribe(it -> {
+                    System.out.println("onNext:" + it + "  thread: " + Thread.currentThread());
+                });
+    }
+
 }
