ab7996601ec28f770b35693936c71f3880bc9523
leleact <leleact@gmail.com>
2019-10-18 14:49:48 +0800

    test(spring-profile): remove incorrect comment
    remove incorrect comment

--- spring/spring-profile/src/main/java/com/github/leleact/test/spring/profiles/ProfileApplication.java
+++ spring/spring-profile/src/main/java/com/github/leleact/test/spring/profiles/ProfileApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProfileApplication {
 
-    // springboot log4j2 会新增一个Filter导致日志打印不出来，在基本环境变量加载完成后删除该filter
-    // 在 jvm 中设置 -Dspring.profiles.active=uat 后，环境变量中设置 spring_profiles_active=sit
-    // 两个都会读取
-    // ConfigFileApplicationListener会再load一遍环境变量中的spring_profiles_active [spring boot 1.5.10]
     public static void main(String[] args) {
         SpringApplication.run(ProfileApplication.class, args);
     }
--- spring/spring-profile/src/test/java/com/github/leleact/test/spring/profiles/TraceLogTests.java
+++ spring/spring-profile/src/test/java/com/github/leleact/test/spring/profiles/TraceLogTests.java
@@ -1,6 +1,7 @@
 package com.github.leleact.test.spring.profiles;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,9 +10,10 @@ public class TraceLogTests {
     private static final Logger logger = LoggerFactory.getLogger(TraceLogTests.class);
 
     @Test
-    public void printLog() {
+    public void printLogTest() {
         if (logger.isTraceEnabled()) {
             logger.trace("xxx");
         }
+        Assertions.assertEquals(1, 1);
     }
 }
