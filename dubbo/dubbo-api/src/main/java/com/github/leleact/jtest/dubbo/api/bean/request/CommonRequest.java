5673af8cb054b096068195fa31a009700467deda
leleact <leleact@gmail.com>
2019-11-07 13:43:32 +0800

    build: use parent package version
    use parent package version

--- dubbo/dubbo-api/src/main/java/com/github/leleact/jtest/dubbo/api/bean/request/CommonRequest.java
+++ dubbo/dubbo-api/src/main/java/com/github/leleact/jtest/dubbo/api/bean/request/CommonRequest.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 public class CommonRequest implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private String s;
 
     private Integer i;
--- dubbo/dubbo-api/src/main/java/com/github/leleact/jtest/dubbo/api/bean/response/CommonResponse.java
+++ dubbo/dubbo-api/src/main/java/com/github/leleact/jtest/dubbo/api/bean/response/CommonResponse.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
 
 public class CommonResponse extends CommonRequest implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private int res;
 
     public int getRes() {
