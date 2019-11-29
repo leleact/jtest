e9855af0df077d2ff5fc885f41cc469f1597b84a
leleact <leleact@gmail.com>
2019-11-29 16:06:58 +0800

    fix(dubbo-compatibility): fix import error after rename packages
    fix import error after rename packages

--- dubbo/dubbo-compatibility/dubbo-compatibility-consumer/src/main/java/com/github/leleact/jtest/dubbo/compatibility/consumer/TestDubboCompatibilityConsumerApplication.java
+++ dubbo/dubbo-compatibility/dubbo-compatibility-consumer/src/main/java/com/github/leleact/jtest/dubbo/compatibility/consumer/TestDubboCompatibilityConsumerApplication.java
@@ -1,6 +1,6 @@
 package com.github.leleact.jtest.dubbo.compatibility.consumer;
 
-import com.lele.test.dubbo.compatibility.api.EchoService;
+import com.github.leleact.jtest.dubbo.compatibility.api.EchoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,9 +11,10 @@ import org.springframework.context.annotation.ImportResource;
 public class TestDubboCompatibilityConsumerApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(TestDubboCompatibilityConsumerApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(TestDubboCompatibilityConsumerApplication.class,
+                                                                      args);
         EchoService demoService = (EchoService) applicationContext.getBean(
-                "demoService"); // get remote service proxy
+            "demoService"); // get remote service proxy
         demoService.echo("11111111111111111"); // call remote method
     }
 
