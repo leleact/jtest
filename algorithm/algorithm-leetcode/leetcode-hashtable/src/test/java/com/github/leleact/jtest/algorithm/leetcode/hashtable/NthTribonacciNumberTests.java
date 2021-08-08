8f9dd876438d8c91c9b67e0300cf037ccf05d161
leleact <leleact@gmail.com>
2021-08-08 19:43:11 +0800

    test(*): leetcode algorithm update
    leetcode algorithm update

---
+++ algorithm/algorithm-leetcode/leetcode-hashtable/src/test/java/com/github/leleact/jtest/algorithm/leetcode/hashtable/NthTribonacciNumberTests.java
@@ -0,0 +1,42 @@
+package com.github.leleact.jtest.algorithm.leetcode.hashtable;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * N-th Tribonacci Number
+ *
+ * @author leleact
+ * @since 2021-08-08
+ */
+@Slf4j
+public class NthTribonacciNumberTests {
+    public static class Solution {
+        private Map<Integer, Integer> store = new HashMap<>();
+
+        public int tribonacci(int n) {
+            if (n == 0) {
+                return 0;
+            }
+
+            if (n == 1) {
+                return 1;
+            }
+
+            if (n == 2) {
+                return 1;
+            }
+
+            Integer stored = store.get(n);
+            if (stored != null) {
+                return stored;
+            }
+
+            int result = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1);
+            store.put(n, result);
+            return result;
+        }
+    }
+}
