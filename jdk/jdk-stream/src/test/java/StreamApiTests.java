c018383985e110f7d3e0327bdf0da9c139c81bb1
leleact <leleact@gmail.com>
2021-09-25 23:06:29 +0800

    test(jdk-stream): jdk stream api test
    jdk stream api test

--- jdk/jdk-stream/src/test/java/StreamApiTests.java
+++ jdk/jdk-stream/src/test/java/StreamApiTests.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 /**
@@ -15,5 +17,15 @@ public class StreamApiTests {
         Assertions.assertArrayEquals(new String[]{"1", "2", "3"}, IntStream.of(1, 2, 3)
                                                                            .mapToObj(String::valueOf)
                                                                            .toArray());
+    }
+
+    @Test
+    public void integerListToIntArrayTest() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        // Integer list convert to int array
+        Assertions.assertArrayEquals(new int[]{1, 2, 3}, list.stream().mapToInt(Integer::valueOf).toArray());
     }
 }
