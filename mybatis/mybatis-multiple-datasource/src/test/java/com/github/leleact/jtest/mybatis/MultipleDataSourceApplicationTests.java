b14c307993cd220f14c743e1efd61c74e0f2dbcc
leleact <leleact@gmail.com>
2022-03-13 00:25:35 +0800

    build(project): spring boot mybatis multiple datasource
    spring boot mybatis multiple datasource

--- mybatis/mybatis-multiple-datasource/src/test/java/com/github/leleact/jtest/mybatis/MultipleDataSourceApplicationTests.java
+++ mybatis/mybatis-multiple-datasource/src/test/java/com/github/leleact/jtest/mybatis/MultipleDataSourceApplicationTests.java
@@ -2,14 +2,11 @@ package com.github.leleact.jtest.mybatis;
 
 import com.github.leleact.jtest.mybatis.db1.mapper.DbMapper;
 import com.github.leleact.jtest.mybatis.db2.mapper.UserMapper;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class MultipleDataSourceApplicationTests {
 
