a4c3b4f6cd62ff983156a74b8b63ceac259361ed
leleact <leleact@gmail.com>
2022-12-07 00:08:12 +0800

    project: update dependency version
    update dependency version

--- jdk/jdk-certificate/src/test/java/com/github/leleact/jtest/jdk/certificate/CertificateInfoTest.java
+++ jdk/jdk-certificate/src/test/java/com/github/leleact/jtest/jdk/certificate/CertificateInfoTest.java
@@ -13,8 +13,7 @@ import java.util.Enumeration;
 public class CertificateInfoTest {
 
     @Test
-    public void getCertificateInfo() throws KeyStoreException, IOException, CertificateException,
-        NoSuchAlgorithmException {
+    public void getCertificateInfo() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
 
         String pfxFileName = "C:\\Users\\Lele\\Desktop\\czbdsf.pfx";
         String pfxPassword = "password";
@@ -28,13 +27,12 @@ public class CertificateInfoTest {
         while (aliases.hasMoreElements()) {
             alias = aliases.nextElement();
             System.out.println("alias[" + alias + "]");
-            if (alias != null)
-                break;
+            if (alias != null) break;
         }
 
         X509Certificate certificate = (X509Certificate) ks.getCertificate(alias);
-        System.out.println("issuer  DN[" + certificate.getIssuerDN() + "]");
-        System.out.println("subject DN[" + certificate.getSubjectDN() + "]");
+        System.out.println("issuer  DN[" + certificate.getIssuerX500Principal().getName() + "]");
+        System.out.println("subject DN[" + certificate.getSubjectX500Principal().getName() + "]");
         System.out.println("sigAlg  DN[" + certificate.getSigAlgName() + "]");
         System.out.println("sigAlgo DN[" + certificate.getSigAlgOID() + "]");
         System.out.println("type      [" + certificate.getType() + "]");
