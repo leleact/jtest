4aa4929f9154e57b7141899077af9cc2ddd928b2
leleact <leleact@gmail.com>
2023-03-03 23:24:50 +0800

    test(servicecomb): servicecomb test
    servicecomb test

---
+++ servicecomb/README.md
@@ -0,0 +1,3 @@
+# TODO
+
+use servicecomb version 1.3.5, only support java 1.8, so failed to test
---
+++ servicecomb/servicecomb-server/src/main/java/com/github/leleact/jtest/servicecomb/server/Main.java
@@ -0,0 +1,9 @@
+package com.github.leleact.jtest.servicecomb.server;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        BeanUtils.init();
+    }
+}
