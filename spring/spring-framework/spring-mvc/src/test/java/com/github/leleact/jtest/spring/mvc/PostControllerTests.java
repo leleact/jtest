3fcff9db6c265157b8a7736fcf280f6339a4b0f2
leleact <leleact@gmail.com>
2023-06-18 22:10:56 +0800

    project: update dependency version
    update dependency version

--- spring/spring-framework/spring-mvc/src/test/java/com/github/leleact/jtest/spring/mvc/PostControllerTests.java
+++ spring/spring-framework/spring-mvc/src/test/java/com/github/leleact/jtest/spring/mvc/PostControllerTests.java
@@ -1,13 +1,13 @@
 package com.github.leleact.jtest.spring.mvc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.Consts;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.message.BasicNameValuePair;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,43 +24,25 @@ class PostControllerTests extends BaseMvcTests {
         reqMap.put("a", "b");
         reqMap.put("c", "d");
         String reqStr = objectMapper.writeValueAsString(reqMap);
-        String response = this.mockMvc.perform(
-            post("/post/map").contentType(MediaType.APPLICATION_JSON_VALUE).content(reqStr))
-                                      .andExpect(status().isOk())
-                                      .andReturn().getResponse().getContentAsString();
+        String response = this.mockMvc.perform(post("/post/map").contentType(MediaType.APPLICATION_JSON_VALUE).content(reqStr)).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
         log.info("response: {}", response);
     }
 
     @Test
     void postQueryForm() throws Exception {
-        String response = this.mockMvc.perform(
-            post("/post/map?a=b&c=d").contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
-                                      .andExpect(status().isOk())
-                                      .andReturn().getResponse().getContentAsString();
+        String response = this.mockMvc.perform(post("/post/map?a=b&c=d").contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
         log.info("response: {}", response);
     }
 
     @Test
     void postUrlEnForm() throws Exception {
-        String response = this.mockMvc.perform(
-            post("/post/map").contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                             .content(EntityUtils.toString(new UrlEncodedFormEntity(Arrays.asList(
-                                 new BasicNameValuePair("a", "b"),
-                                 new BasicNameValuePair("c", "c")), Consts.UTF_8
-                             ))))
-                                      .andExpect(status().isOk())
-                                      .andReturn().getResponse().getContentAsString();
+        String response = this.mockMvc.perform(post("/post/map").contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE).content(EntityUtils.toString(new UrlEncodedFormEntity(Arrays.asList(new BasicNameValuePair("a", "b"), new BasicNameValuePair("c", "c")), StandardCharsets.UTF_8)))).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
         log.info("response: {}", response);
     }
 
     @Test
     void postParamForm() throws Exception {
-        String response = this.mockMvc.perform(
-            post("/post/map").contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                             .param("a", "b")
-                             .param("c", "d"))
-                                      .andExpect(status().isOk())
-                                      .andReturn().getResponse().getContentAsString();
+        String response = this.mockMvc.perform(post("/post/map").contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE).param("a", "b").param("c", "d")).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
         log.info("response: {}", response);
     }
 }
