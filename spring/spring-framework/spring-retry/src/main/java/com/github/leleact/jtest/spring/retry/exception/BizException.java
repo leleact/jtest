ff6bcbab06d97fa5d3dde01e4f7a1a2b2da11d17
leleact <leleact@gmail.com>
2023-07-29 22:17:06 +0800

    fix(spring-retry): exception serialVersionUID
    exception serialVersionUID

--- spring/spring-framework/spring-retry/src/main/java/com/github/leleact/jtest/spring/retry/exception/BizException.java
+++ spring/spring-framework/spring-retry/src/main/java/com/github/leleact/jtest/spring/retry/exception/BizException.java
@@ -7,6 +7,9 @@ package com.github.leleact.jtest.spring.retry.exception;
  * @since 2023-07-28
  */
 public class BizException extends RuntimeException {
+    @java.io.Serial
+    private static final long serialVersionUID = -6554274737825257159L;
+
     public BizException() {
         super();
     }
