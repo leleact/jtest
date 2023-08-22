bfc4e813e90804359a3b8f6b86eb08cc2cb551fb
leleact <leleact@gmail.com>
2023-08-22 22:12:47 +0800

    chore: rename package
    rename package

---
+++ quick-test/src/test/java/com/github/leleact/jtest/quick/test/QuickTests.java
@@ -0,0 +1,12 @@
+package com.github.leleact.jtest.quick.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class QuickTests {
+    @Test
+    public void mainTest() {
+        log.info("mainTest start");
+    }
+}
