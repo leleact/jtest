5d0c1900933784be1d7a4182ee96ee98c0bdf48b
leleact <leleact@gmail.com>
2024-07-07 19:26:25 +0800

    test(jdk-stream): parallel java stream api
    parallel java stream api

--- jdk/jdk-stream/src/test/java/StreamApiTests.java
+++ jdk/jdk-stream/src/test/java/StreamApiTests.java
@@ -1,9 +1,11 @@
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * jdk stream api test
@@ -11,12 +13,12 @@ import java.util.stream.IntStream;
  * @author leleact
  * @since 2021-09-15
  */
+@Slf4j
 public class StreamApiTests {
     @Test
     public void mapToObjTest() {
-        Assertions.assertArrayEquals(new String[]{"1", "2", "3"}, IntStream.of(1, 2, 3)
-                                                                           .mapToObj(String::valueOf)
-                                                                           .toArray());
+        Assertions.assertArrayEquals(new String[]{"1", "2", "3"},
+            IntStream.of(1, 2, 3).mapToObj(String::valueOf).toArray());
     }
 
     @Test
@@ -27,5 +29,13 @@ public class StreamApiTests {
         list.add(3);
         // Integer list convert to int array
         Assertions.assertArrayEquals(new int[]{1, 2, 3}, list.stream().mapToInt(Integer::valueOf).toArray());
+    }
+
+    @Test
+    public void listStreamParallelTest() {
+        Stream.of("1", "2", "3").parallel().forEach(s -> {
+            // breakpoint
+            log.info("{}", s);
+        });
     }
 }
